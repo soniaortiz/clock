577d45dc58076f962e8f4cd77bb515aae0239bc7
Sonia <sonia@Sonias-MacBook-Air.local>
2020-09-10 18:58:15 -0500

    create clock and timer

---
+++ src/components/Clock.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+const second = 1000;
+export class Clock extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentTime: new Date().toLocaleString(),
+            timer: second * 10
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                currentTime: new Date().toLocaleString()
+            })
+        }, 1000)
+    }
+
+    set10SecTimer = () => {
+        if (this.state.timer > 0) {
+            setInterval(() => {
+                this.setState({
+                    timer: this.state.timer - second
+                })
+            }, second)
+        }
+    }
+
+    render() {
+        return (<div>
+            <h1>Current time</h1>
+            <div>{this.state.currentTime.toLocaleString()}</div>
+            {
+                <h3>{
+                    this.state.timer === 0 ? `Time's up` : ''
+                }</h3>
+            }
+            <button
+                onClick={this.set10SecTimer}
+            >Set timer for 10 seconds</button>
+        </div>)
+    }
+}
+
